f5045776308af9c0e0177eccd5071581cd4ccf95
HuangYihang <1992831629@qq.com>
2026-06-15 14:28:28 +0800

    修改鉴权

--- Backend_cs/Controllers_Sys/FormController.cs
+++ Backend_cs/Controllers_Sys/FormController.cs
@@ -1,6 +1,7 @@
 namespace PTools_PSilo.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using T2ACore;
 
 /// <summary>
@@ -26,9 +27,32 @@ public class FormController : ControllerBase
     /// </summary>
     /// <returns>当前用户是管理员时返回true。</returns>
     [HttpPost]
-    public TPResponse<bool> IsAdmin()
+    public TPResponse<bool> IsAdmin([FromServices] IAuthBusiness authBusiness)
     {
         var (account, _) = ScopeUser.GetUserAccountName();
-        return TPResponse.New(Config.BaseConfig.AdminList.Contains(account));
+        var roles = authBusiness.GetRolesListForUser(account);
+
+        var adminRoles = roles.FindAll(role => role.UpRole == "Admin");
+        var hasAdminRole = adminRoles.Count > 0;
+
+        var adminRoleChecks = new List<(
+            RoleList Role,
+            List<Auth_role_Class> SysRoles,
+            bool RoleCodeMatched,
+            bool RegionMatched)>();
+
+        foreach (var role in adminRoles)
+        {
+            var sysRoles = authBusiness.SearchBy_RoleCode_Tenant(role.UpRole, role.Tenant);
+            var roleCodeMatched = sysRoles.Exists(sysRole => sysRole.Role_code == role.UpRole);
+            var regionMatched = sysRoles.Exists(sysRole =>
+                sysRole.Role_code == role.UpRole &&
+                sysRole.Region == role.Region);
+
+            adminRoleChecks.Add((role, sysRoles, roleCodeMatched, regionMatched));
+        }
+
+        var isAdmin = hasAdminRole && adminRoleChecks.Exists(check => check.RegionMatched);
+        return TPResponse.New(isAdmin);
     }
 }
